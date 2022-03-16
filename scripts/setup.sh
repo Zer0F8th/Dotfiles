3b366511748cceaee8f818e27d78153dece6e3b2
Zer0F8th <25908759+Zer0F8th@users.noreply.github.com>
2022-03-16 15:07:33 -0400

    fix: fixed oh my zsh plugins from not installing
    Fixed oh my zsh plugins from not installing and move .zshrc into its own folder in the config/ dir.

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+###########################################
+#----------) Terminal Setup (-------------#
+###########################################
 
+# Check which distro is running
 if [[ -f /etc/os-release ]]; then
   # freedesktop.org and systemd
   . /etc/os-release
@@ -12,56 +16,11 @@ else
   VER=$(uname -r)
 fi
 
-if [[ $OS == "Ubuntu" ]]; then
-  # Update the system and install dependencies
-  sudo apt-get update &&
-    sudo apt-get upgrade -y &&
-    sudo apt-get install -y \
-      git \
-      zsh \
-      vim \
-      zoom \
-      terminator \
-      build-essential \
-      python-dev \
-      python-pip \
-      python-virtualenv \
-      software-properties-common \
-      apt-transport-https \
-      wget
-
-  curl -sSL https://install.python-poetry.org | python3 - &&
-    wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add - &&
-    sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" &&
-    sudo apt-get update &&
-    sudo apt-get install -y code
-  # Install Oh My Zsh
-  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" --unattended
-
-  # Install zsh-autosuggestions
-  git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}"/plugins/zsh-autosuggestions
-
-  # Install zsh-syntax-highlighting
-  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}"/plugins/zsh-syntax-highlighting
-
-  # Install spaceship prompt
-  git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
-
-  # Configure terminator
-
-  # Make the terminator config directory only if it doesn't already exist
-  mkdir -p "$HOME"/.config/terminator
-
-  # Copy the terminator config file to the terminator config directory
-  cp ../.config/terminator/config "$HOME"/.config/terminator/config
-
-  # Replace the terminator desktop file with the one from the repo
-  sudo cp -rf ../config/terminator/terminator.desktop /usr/share/applications/
-fi
-
 if [[ $OS == "Fedora Linux" ]]; then
   echo "Fedora Linux"
   echo "================================"
+  # Set the Oh My Zsh custom directory
+  ZSH_CUSTOM="$HOME"/.oh-my-zsh/custom
   # Update the system and install dependencies
   sudo dnf update &&
     sudo dnf upgrade -y &&
@@ -72,7 +31,8 @@ if [[ $OS == "Fedora Linux" ]]; then
       wget \
       terminator \
       python3-pip \
-      python3-virtualenv
+      python3-virtualenv \
+      util-linux-user
 
   # Install Discord
   sudo dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
@@ -113,15 +73,15 @@ if [[ $OS == "Fedora Linux" ]]; then
   bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
   # Install zsh-autosuggestions
-  git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}"/plugins/zsh-autosuggestions
+  git clone https://github.com/zsh-users/zsh-autosuggestions "$ZSH_CUSTOM"/plugins/zsh-autosuggestions
 
   # Install zsh-syntax-highlighting
-  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}"/plugins/zsh-syntax-highlighting
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$ZSH_CUSTOM"/plugins/zsh-syntax-highlighting
 
   # Install spaceship prompt
   git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
 
   # Copy the zshrc file to the home directory, replacing the original
-  cp -rf ../.zshrc "$HOME"/.zshrc
+  cp -rf ../config/zshrc/.zshrc "$HOME"/.zshrc
 
 fi
