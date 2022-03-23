6d2c8c65328c533a3b7fc9b174417280fd621ef7
Zer0F8th <25908759+Zer0F8th@users.noreply.github.com>
2022-03-23 16:08:11 -0400

    fix: last fix didn't fix, this should fix

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -30,20 +30,20 @@ if [[ $OS == "Fedora Linux" ]]; then
   # Update the system and install dependencies
 
   sudo dnf check-update
-  sudo dnf upgrade -y
-  sudo dnf install -y git \
-    zsh \
-    vim \
-    wget \
-    htop \
-    terminator \
-    python3-pip \
-    python3-virtualenv \
-    util-linux-user \
-    gnome-tweaks \
-    gtk-murrine-engine \
-    gnome-shell-extensions \
-    stacer
+  sudo dnf upgrade -y &&
+    sudo dnf install -y git \
+      zsh \
+      vim \
+      wget \
+      htop \
+      terminator \
+      python3-pip \
+      python3-virtualenv \
+      util-linux-user \
+      gnome-tweaks \
+      gtk-murrine-engine \
+      gnome-extensions-app \
+      stacer
 
   # Install Discord
   sudo dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-"$(rpm -E %fedora)".noarch.rpm -y
@@ -130,8 +130,6 @@ if [[ $OS == "Fedora Linux" ]]; then
   }
   rm -rf Orchis-theme
   rm -rf Tela-circle-icon-theme
-
-  # Install Tela Circle Icons
 
   # Install dnf automatic updates
   if [[ $AUTOMATIC_UPDATES == true ]]; then
